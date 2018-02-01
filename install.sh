2eab439808032902eb3ca8341c99841da399d599
controllinghand <31561227+controllinghand@users.noreply.github.com>
2018-01-31 23:53:07 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,23 @@
 # install.sh
 # Checks smartnode on Ubuntu 17.10 x64
 # ATTENTION: The anti-ddos part will disable http, https and dns ports.
+# Controllinghand version 1.1 1/31/2018
+
+# Added LithMage Check 
+if ! (dpkg -s smartcashd | grep -F "ok installed") &>/dev/null; then
+	echo "WARNING: These scripts uses PPA installed wallet commands."
+	echo "FAILED TO DETECT such wallet commands"
+	printf "Press Ctrl+C to cancel or Enter install scripts anyway: "
+	read IGNORE
+fi
+
+# Check to see if smartnode directory already exist
+if [ -d ~/smartnode ]
+then
+    echo "~/smartnode Directory already exist"
+    printf "Press Ctrl+C to cancel or Enter to clean up and reinstall:"
+    read IGNORE
+fi
 
 # Warning that the script will reboot the server
 echo "WARNING: This script will reboot the server when it's finished."
@@ -31,19 +48,24 @@ wget https://rawgit.com/controllinghand/smartnode-checkonly/master/clearlog.sh
 
 # Create a cronjob for making sure smartcashd is always running
 # SLG changed to every 10 mins to help keep it from stepping on setting up nodes and upgrades etc
-(crontab -l ; echo "*/10 * * * * ~/smartnode/makerun.sh") | crontab -
+# (crontab -l ; echo "*/10 * * * * ~/smartnode/makerun.sh") | crontab -
+# Added LithMage method to keep from multiple entries
+(crontab -l 2>/dev/null | grep -v -F "smartnode/makerun.sh" ; echo "*/1 * * * * ~/smartnode/makerun.sh" ) | crontab -
 chmod 0700 ./makerun.sh
 
 # Create a cronjob for making sure the daemon is never stuck
-(crontab -l ; echo "*/30 * * * * ~/smartnode/checkdaemon.sh") | crontab -
+# (crontab -l ; echo "*/30 * * * * ~/smartnode/checkdaemon.sh") | crontab -
+(crontab -l 2>/dev/null | grep -v -F "smartnode/checkdaemon.sh" ; echo "*/30 * * * * ~/smartnode/checkdaemon.sh" ) | crontab -
 chmod 0700 ./checkdaemon.sh
 
 # Create a cronjob for making sure smartcashd is always up-to-date
-(crontab -l ; echo "*/120 * * * * ~/smartnode/upgrade.sh") | crontab -
+# (crontab -l ; echo "*/120 * * * * ~/smartnode/upgrade.sh") | crontab -
+(crontab -l 2>/dev/null | grep -v -F "smartnode/upgrade.sh" ; echo "*/120 * * * * ~/smartnode/upgrade.sh" ) | crontab -
 chmod 0700 ./upgrade.sh
 
 # Create a cronjob for clearing the log file
-(crontab -l ; echo "0 0 */2 * * ~/smartnode/clearlog.sh") | crontab -
+# (crontab -l ; echo "0 0 */2 * * ~/smartnode/clearlog.sh") | crontab -
+(crontab -l 2>/dev/null | grep -v -F "smartnode/clearlog.sh" ; echo "0 0 */2 * * ~/smartnode/clearlog.sh" ) | crontab -
 chmod 0700 ./clearlog.sh
 
 # Change the SSH port
@@ -51,10 +73,13 @@ chmod 0700 ./clearlog.sh
 # sed -i "s/14855/${_sshPortNumber}/g" ~/smartnode/anti-ddos.sh
 
 # Run the anti-ddos script You will need to put in your sudo password
+echo "Installing anti-ddos script requires sudo password or root."
+printf "Press Ctrl+C to cancel or Enter to continue:  "
+read IGNORE
 sudo bash ./anti-ddos.sh
 
 # Reboot the server
 echo "Rebooting the server so that changes will take effect."
-printf "Enter to continue: "
+printf "Press Ctrl+C to cancel or Enter to continue:  "
 read IGNORE
 sudo reboot
